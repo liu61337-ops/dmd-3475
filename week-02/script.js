41168751be7dd58eaf2db23d75fb37fc95fb71c8
Liang-Yu Liu <lyl23001@uconn.edu>
2026-01-29 11:50:40 -0500

    This is my week 2 code.

---
+++ week-02/script.js
@@ -0,0 +1,24 @@
+var week = 2;
+
+console.log("Hello, this is Week" + week + ".");
+
+//alert("hello, this is really week 2");
+
+var Today = "Thursday";
+//console.log(today);
+var Tomorrow = "Friday";
+
+//console.log("Today is Thursday");
+//console.log("Tomorrow is Friday");
+
+var greeting = "Today is " + Today + ".";
+
+console.log(greeting);
+
+var nextweek = week + 1;
+console.log("Next week will be Week " + nextweek);
+
+//var myName = "Sophie";
+
+var myName = prompt("What is your name?");
+alert("My name is " + myName);
